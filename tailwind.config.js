a0544dda3ee05c7822567825d7d813d29a7cb476
Mortimer Marsyja <marcoscolladosegura@yandex.com>
2026-07-17 12:54:01 +0000

    style: update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      textShadow: {
+        '3d': `
+          1px 1px 0px #e2cca6,
+          2px 2px 0px #e2cca6,
+          3px 3px 0px #e2cca6,
+          4px 4px 0px #e2cca6,
+          5px 5px 0px #e2cca6,
+          6px 6px 0px #e2cca6,
+          7px 7px 0px #310c0c, /* Borde final oscuro para dar profundidad */
+          8px 8px 0px #310c0c
+        `,
+      },
       colors: {
         "canyon-sand": "#eceff1",
         "soft-cream": "#eceff1",
